aa69ce9df0d6024f7db3adfddc77f56c91e47cad
Emmanuel-mry <54974028+Emmanuel-mry@users.noreply.github.com>
2019-09-28 23:23:01 -0700

    Add files via upload

---
+++ Principal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_Consola
+{
+    class Principal
+    {
+        public void CapturarDatos()
+        {
+            //Declaracion de variables locales
+            int Totalcentavos; 
+            int Pesos = 0;
+            int Centavos = 0;
+
+            Console.WriteLine("INGRESE LOS CENTAVOS QUE DESEA CONVERTIR"); //Capturando los centavos 
+            Console.WriteLine();
+            Totalcentavos = Convert.ToInt32(Console.ReadLine());
+
+            Convertir(Totalcentavos, out Pesos, out Centavos); //declaracion de out
+            Console.WriteLine("PESOS:"+" "+Pesos);
+            Console.WriteLine("CENTAVOS"+" "+ Centavos);
+            Console.ReadKey();
+
+
+
+        }
+
+        public void Convertir(int TotalCentavos, out int Pesos, out int Centavos) //Declaracion del out
+        {
+            //Aqui se realizan las operaciones
+            Pesos = TotalCentavos / 100;
+            Centavos = TotalCentavos % 100;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //No estaba seguro si era necesario la encapsulacion de datos porque en la forma no se utilizo
+            Principal PR = new Principal(); //Instanciando la clase Principal
+            PR.CapturarDatos(); //llamando al metodo CapturarDatos
+            
+                
+            
+        }
+    }
+}
